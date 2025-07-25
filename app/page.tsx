6ecc76074febc783b6b044f0d00281fa9c585457
Sameer <sameer.edu19@gmail.com>
2025-07-25 12:17:54 +0530

    added middleware

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-
+      <Link href={"/admin/auth/signin"}>
+        signin
+      </Link>
     </>
   );
 }
---
+++ routes.ts
@@ -0,0 +1,13 @@
+export const publicRoutes = [
+    "/"
+]
+
+export const authRoutes = [
+    "/admin/auth/signup",
+    "admin/auth/signin"
+]
+
+export const apiAuthPrefix = "/api/auth";
+
+export const DEFAULT_LOGIN_REDIRECT = '/settings';
+
